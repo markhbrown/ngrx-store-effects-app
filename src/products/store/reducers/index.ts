09aa089ededc3a3fc7e244e7758ab2ef8e454afb
Mark Brown <markbrown83@outlook.com>
2017-12-05 18:23:25 +0000

    current progress through tutorials

---
+++ src/products/store/reducers/index.ts
@@ -0,0 +1,14 @@
+import * as fromPizzas from "./pizzas.reducer";
+import { ActionReducerMap, createFeatureSelector } from "@ngrx/store";
+
+export interface ProductsState {
+  pizzas: fromPizzas.PizzaState;
+}
+
+export const reducers: ActionReducerMap<ProductsState> = {
+  pizzas: fromPizzas.reducer
+};
+
+export const getProductsState = createFeatureSelector<ProductsState>(
+  "products"
+);
