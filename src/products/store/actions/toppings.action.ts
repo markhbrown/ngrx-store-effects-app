f56be5755dca7c6c8bf42fc3f262e5548490a66f
Mark Brown <markbrown83@outlook.com>
2017-12-06 09:19:42 +0000

    further progress made on the course

--- src/products/store/actions/toppings.action.ts
+++ src/products/store/actions/toppings.action.ts
@@ -2,9 +2,11 @@ import { Action } from "@ngrx/store";
 
 import { Topping } from "../../models/topping.model";
 
+// Toppings
 export const LOAD_TOPPINGS = "[Products] Load Toppings";
 export const LOAD_TOPPINGS_FAIL = "[Products] Load Toppings Fail";
 export const LOAD_TOPPINGS_SUCCESS = "[Products] Load Toppings Success";
+export const VISUALISE_TOPPINGS = "[Products] Visualise Toppings";
 
 export class LoadToppings implements Action {
   readonly type = LOAD_TOPPINGS;
@@ -16,8 +18,18 @@ export class LoadToppingsFail implements Action {
 }
 
 export class LoadToppingsSuccess implements Action {
-    readonly type = LOAD_TOPPINGS_SUCCESS;
-    constructor(public payload: Topping[]) {}
-  }
+  readonly type = LOAD_TOPPINGS_SUCCESS;
+  constructor(public payload: Topping[]) {}
+}
 
-  export type ToppingsAction = LoadToppings | LoadToppingsFail | LoadToppingsSuccess;
+export class VisualiseToppings implements Action {
+  readonly type = VISUALISE_TOPPINGS;
+  constructor(public payload: number[]) {}
+}
+
+// action types
+export type ToppingsAction =
+  | LoadToppings
+  | LoadToppingsFail
+  | LoadToppingsSuccess
+  | VisualiseToppings;
--- src/products/store/selectors/pizzas.selectors.ts
+++ src/products/store/selectors/pizzas.selectors.ts
@@ -3,6 +3,7 @@ import { createSelector } from "@ngrx/store";
 import * as fromRoot from "../../../app/store";
 import * as fromFeature from "../reducers";
 import * as fromPizzas from "../reducers/pizzas.reducer";
+import * as fromToppings from "../reducers/toppings.reducer";
 import { Pizza } from "../../models/pizza.model";
 
 export const getPizzaState = createSelector(
@@ -19,7 +20,18 @@ export const getSelectedPizza = createSelector(
   getPizzasEntities,
   fromRoot.getRouterState,
   (entities, router): Pizza => {
-      return router.state && entities[router.state.params.pizzaId]
+    return router.state && entities[router.state.params.pizzaId];
+  }
+);
+
+export const getPizzaVisualised = createSelector(
+  getSelectedPizza,
+  fromToppings.getToppingsEntities,
+  fromToppings.getSelectedToppings,
+  (pizza, toppingEntities, selectedToppings) => {
+    const toppings = selectedToppings.map(id => toppingEntities[id]);
+
+    return { ...pizza, toppings };
   }
 );
 
